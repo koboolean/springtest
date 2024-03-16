2457bed4e76fc7ef0b271573cc38415c18e0a251
조현준 <45157196+koboolean@users.noreply.github.com>
2024-03-17 00:04:55 +0900

    Mockito 값을 가져온 후 데이터 검증 수행

--- src/main/java/com/koboolean/test/springtest/web/response/ExamPassStudentResponse.java
+++ src/main/java/com/koboolean/test/springtest/web/response/ExamPassStudentResponse.java
@@ -1,9 +1,12 @@
 package com.koboolean.test.springtest.web.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 @AllArgsConstructor
 public class ExamPassStudentResponse {
     private final String studentName;
--- src/test/java/com/koboolean/test/springtest/service/StudentScoreServiceTest.java
+++ src/test/java/com/koboolean/test/springtest/service/StudentScoreServiceTest.java
@@ -4,9 +4,13 @@ import com.koboolean.test.springtest.model.StudentPass;
 import com.koboolean.test.springtest.repository.StudentFailRepository;
 import com.koboolean.test.springtest.repository.StudentPassRepository;
 import com.koboolean.test.springtest.repository.StudentScoreRepository;
+import com.koboolean.test.springtest.web.response.ExamPassStudentResponse;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -66,5 +70,41 @@ class StudentScoreServiceTest {
         Mockito.verify(studentScoreRepository, Mockito.times(1)).save(Mockito.any());
         Mockito.verify(studentPassRepository, Mockito.times(1)).save(Mockito.any());
         Mockito.verify(studentFailRepository, Mockito.times(0)).save(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("합격자 명단 가져오기 검증")
+    public void getPassListTest(){
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
+
+        String testExam = "testExam";
+
+        StudentPass pass1 = StudentPass.builder().studentPassId(1L).studentName("koboolean").exam(testExam).avgScore(70.0).build();
+        StudentPass pass2 = StudentPass.builder().studentPassId(2L).studentName("test").exam(testExam).avgScore(80.0).build();
+        StudentPass pass3 = StudentPass.builder().studentPassId(3L).studentName("koboolean").exam("secondTestExam").avgScore(90.0).build();
+
+        // findAll 값을 보정할 수 있다. service.getPassStudentList의 findAll을 보정하여 하단의 세개의 값이 결과로 나오도록 하는것이다.
+        Mockito.when(studentPassRepository.findAll()).thenReturn(List.of(
+                pass1, pass2, pass3
+        ));
+
+        StudentScoreService service = new StudentScoreService(
+                studentScoreRepository, studentPassRepository, studentFailRepository
+        );
+
+        // when
+        var expectResponse = List.of(pass1, pass2)
+                .stream()
+                .map((pass) -> new ExamPassStudentResponse(pass.getStudentName(), pass.getAvgScore()))
+                .toList();
+
+        List<ExamPassStudentResponse> responses = service.getPassStudentList("testExam");
+
+        // 같은 값이 나오는지 검증
+        Assertions.assertEquals(expectResponse, responses);
+
+        responses.forEach((r) -> System.out.println(r.getStudentName()+ " : " + r.getAvgScore()));
     }
 }
